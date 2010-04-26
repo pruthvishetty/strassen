a4a0640a97e455145325e2a82fc8064eb533e64b
kagzter <kagzter@f646d0ac-a201-c439-b55e-bc6ef97af6c5>
2010-04-26 18:37:53 +0000

    Expanded on the function list for the parallel c implementation of our matlab multiplier code

--- trunk/mpstrassen.c
+++ trunk/mpstrassen.c
@@ -53,4 +53,45 @@ int main (int argc, char *argv[]){
     B[i][i] = 1.0 + A[i][i];
 
   //start timer
+  clock_gettime(CLOCK_REALTIME,&start);
+  
+  #pragma omp parallel shared(A,B,C,chunk) private(a,b,c)
+  {
+     #pragma omp for schedule(dynamic,chunk) nowait
+      for (b=0; b<DIM_K; b++){
+          for (a=0; a<DIM_I; a++){
+              for (c=0; c<DIM_J; c++){
+                 C[a][b] += A[a][c] * B[c][b];
+              }
+          }
+      }
+  }
+
+  //stop timer
+  clock_gettime(CLOCK_REALTIME,&finish);
+
+  //calculate time taken
+  ntime = finish.tv_nsec - start.tv_nsec;
+  stime = (long) finish.tv_sec - (long) start.tv_sec;
+  total_time = (double) stime + ((double) ntime)/1e9;
+  printf("Time taken = %lf \n", (double) stime + ((double) ntime)/1e9);
+}
+/****************************************************************************
+* Note that all following functions only deal with square matrices 
+* where N is divisible by 2.
+*****************************************************************************/
+void addMatrices(double **x, double **y, double **z, int size){
+//performs a matrix addition operation, z=x+y
+}
+void subMatrices(double **x, double **y, double **z, int size){
+//performs a matrix subtraction operation, z=x-y
+}
+void splitMatrix(double **a, double**a11, **a12, **a21, **a22, int size){
+//takes a matrix a adn splits it into its 4 quadrants.
+}
+void catMatrix(double **a, double**a11, **a12, **a21, **a22, int size){
+//does the inverse of the splitMatrix function
+}
+void multMatrix(double **x, double **y, double **z, int size){
+//multiplys two matrices: z=x*y
 }
