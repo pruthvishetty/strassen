2352b28b0e5a8b7d46bc4f4401f76f5e09078d5b
kagzter <kagzter@f646d0ac-a201-c439-b55e-bc6ef97af6c5>
2010-05-02 02:05:52 +0000

    Added the 2nd progress report and the powerpoint presentation

--- trunk/matrix.c
+++ trunk/matrix.c
@@ -11,15 +11,15 @@
 #include <sys/types.h>
 
 //matrix dimensions
-#define DIM_I 1000
-#define DIM_J 1000
+#define DIM_N 1000
+#define DIM_M 1000
 #define DIM_K 1000
 #define STEP 10
 //other stuff
 double sum, snorm;
 double *d;
 //matrices
-double A[DIM_I][DIM_J], B[DIM_J][DIM_K], C[DIM_I][DIM_K];
+double A[DIM_N][DIM_M], B[DIM_M][DIM_K], C[DIM_N][DIM_K];
 
 int main (int argc, char *argv[])
 {
@@ -30,29 +30,29 @@ int main (int argc, char *argv[])
    int total_time;
 
    //create the matrices in question
-   d = (double*) malloc(sizeof(double)*DIM_J);
+   d = (double*) malloc(sizeof(double)*DIM_M);
    sum = 0.0; snorm = 0.0;
-   for (i = 0; i < DIM_J; i++){
+   for (i = 0; i < DIM_M; i++){
       d[i] = drand48();
       snorm += d[i]*d[i];
    }
-   for (i = 0; i < DIM_J; i++){
+   for (i = 0; i < DIM_M; i++){
       d[i] = d[i]/sqrt(snorm);
       sum += d[i]*d[i];
    }
 
-   for (i = 0; i < DIM_I; i++)
-      for (j = 0; j < DIM_J; j++)
+   for (i = 0; i < DIM_N; i++)
+      for (j = 0; j < DIM_M; j++)
       A[i][j] =  -2*d[i]*d[j];
 
-   for (i = 0; i < DIM_I; i++)
+   for (i = 0; i < DIM_N; i++)
       A[i][i] = 1.0 + A[i][i];
 
-   for (j = 0; j < DIM_J; j++)
+   for (j = 0; j < DIM_M; j++)
       for (k = 0; k < DIM_K; k++)
       B[j][k] = -2*d[j]*d[k];
 
-   for (j = 0; j < DIM_J; j++)
+   for (j = 0; j < DIM_M; j++)
       B[j][j] = 1.0 + B[j][j];
 
   //start timer                           
@@ -62,8 +62,8 @@ int main (int argc, char *argv[])
   {
      #pragma omp for schedule(dynamic,chunk) nowait
       for (b=0; b<DIM_K; b++){
-          for (a=0; a<DIM_I; a++){
-              for (c=0; c<DIM_J; c++){
+          for (a=0; a<DIM_N; a++){
+              for (c=0; c<DIM_M; c++){
                  C[a][b] += A[a][c] * B[c][b];
               }
           }
@@ -78,7 +78,7 @@ int main (int argc, char *argv[])
   stime = (long) finish.tv_sec - (long) start.tv_sec;
   total_time = (double) stime + ((double) ntime)/1e9;
   printf("Time taken = %lf \n", (double) stime + ((double) ntime)/1e9);
-/*  for (i = 0; i < DIM_I; i++){
+/*  for (i = 0; i < DIM_N; i++){
      for(j = 0; j < DIM_K; j++){
          printf(" %lf", C[i][j]);}
      printf("\n\r");
