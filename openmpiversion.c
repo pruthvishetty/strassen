8c1c1b16fc18784c9b78687ae7817a5cac29903a
kagzter <kagzter@f646d0ac-a201-c439-b55e-bc6ef97af6c5>
2010-05-04 18:59:47 +0000

    OpenMPI version of the strassen multiplication algorithm. Way way faster

--- openmpiversion.c
+++ openmpiversion.c
@@ -1,29 +1,30 @@
 /* 
 Compile options
-	gcc -lrt -lm -fompenmp
+/usr/lib/openmpi/1.4-gcc/bin/mpicc -fopenmp
+/usr/lib/openmpi/1.4-gcc/bin/mpirun --mca btl tcp,self,sm --hostfile my_hostfile -np 7 a.out
 */
 
 #include <omp.h>
+#include <mpi.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 #include <time.h>
 #include <string.h>
-#include <sys/time.h>
-#include <sys/types.h>
+//#include <sys/time.h>
+//#include <sys/types.h>
 
 //matrix dimensions
-#define DIM_N 8192
-#define threads 1
-#define threshold 1024
-int chunk=10;
+#define DIM_N 2048
+#define threads 4
+#define threshold 64
+int chunk=1;
 
 //other stuff
 double sum, snorm;
 double *d;
 
 //matrices
-//double A[DIM_N][DIM_N], B[DIM_N][DIM_N], C[DIM_N][DIM_N];
 double **A, **B, **C;
 
 //Prototypes
@@ -36,9 +37,21 @@ void addMatrices(double**, double**, double**, int);
 
 //MAIN
 int main (int argc, char *argv[]){
-  long stime, ntime;
-  struct timespec start, finish;
-  int i,j,k;
+  int nproc, myid, newsize;
+  int i, j, k, a, b, c, step;
+  int prev, next, start;
+  double stime, ntime;
+  double **a11, **a22, **a12, **a21;
+  double **b11, **b22, **b12, **b21;
+  double **m1, **m2, **m3, **m4, **m5, **m6, **m7;
+  double **t1, **t2, **t3, **t4, **t5, **t6, **t7, **t8, **t9, **t10;
+  //Start the MPI session
+  MPI_Init(&argc,&argv);
+
+  MPI_Comm_size(MPI_COMM_WORLD, &nproc);
+  MPI_Comm_rank(MPI_COMM_WORLD, &myid);
+  
+  newsize = (int)DIM_N/2;
   
   A = (double**) malloc(sizeof(double)*DIM_N);
   for (i = 0; i< DIM_N; i++){
@@ -52,84 +65,201 @@ int main (int argc, char *argv[]){
   for (i = 0; i< DIM_N; i++){
     C[i] = (double*) malloc(sizeof(double)*DIM_N);
   }
-  
-  d = (double*) malloc(sizeof(double)*DIM_N);
-  sum = 0.0; snorm = 0.0;
-  for (i = 0; i < DIM_N; i++){
-    d[i] = drand48();
-    snorm += d[i]*d[i];
-  }
-  for (i = 0; i < DIM_N; i++){
-    d[i] = d[i]/sqrt(snorm);
-    sum += d[i]*d[i];
-  }
+  if (myid == 0){
+    d = (double*) malloc(sizeof(double)*DIM_N);
+    sum = 0.0; snorm = 0.0;
+    for (i = 0; i < DIM_N; i++){
+      d[i] = drand48();
+      snorm += d[i]*d[i];
+    }
+    for (i = 0; i < DIM_N; i++){
+      d[i] = d[i]/sqrt(snorm);
+      sum += d[i]*d[i];
+    }
 
-  for (i = 0; i < DIM_N; i++)
+    for (i = 0; i < DIM_N; i++)
+      for (j = 0; j < DIM_N; j++)
+      A[i][j] =  -2*d[i]*d[j];
+
+    for (i = 0; i < DIM_N; i++)
+      A[i][i] = 1.0 + A[i][i];
+
     for (j = 0; j < DIM_N; j++)
-    A[i][j] =  -2*d[i]*d[j];
+      for (k = 0; k < DIM_N; k++)
+      B[j][k] = -2*d[j]*d[k];
 
-  for (i = 0; i < DIM_N; i++)
-    A[i][i] = 1.0 + A[i][i];
-
-  for (j = 0; j < DIM_N; j++)
-    for (k = 0; k < DIM_N; k++)
-    B[j][k] = -2*d[j]*d[k];
-
-  for (j = 0; j < DIM_N; j++)
-    B[j][j] = 1.0 + B[j][j];
-
-  printf("Num Threads = %d\n",threads);
-  //start timer
-  clock_gettime(CLOCK_REALTIME,&start);
+    for (j = 0; j < DIM_N; j++)
+      B[j][j] = 1.0 + B[j][j];
   
-  strassenMultMatrix(A,B,C,DIM_N);
-
-  //stop timer
-  clock_gettime(CLOCK_REALTIME,&finish);
-  
-  /*for (i=0; i<DIM_N; i++){
-    for (j=0; j<DIM_N; j++)
-      printf("%lf ",C[i][j]);
-    printf("\n");
-  }*/
-
-  //calculate time taken
-  ntime = finish.tv_nsec - start.tv_nsec;
-  stime = (long) finish.tv_sec - (long) start.tv_sec;
-  printf("Strassen Time taken = %lf \n", (double) stime + ((double) ntime)/1e9);
-  
-  
-
-  //start timer
-  clock_gettime(CLOCK_REALTIME,&start);
-  
-  #pragma omp parallel shared(A,B,C,chunk) private(i,j,k) num_threads(threads)
-	{
-	  //multiplication process
-    #pragma omp for schedule(dynamic) nowait
-	    for (j = 0; j < DIM_N; j++){
-		    for (i = 0; i < DIM_N; i++){
-		      C[i][j] = 0.0;
-			    for (k = 0; k < DIM_N; k++)
-				    C[i][j] += A[i][k] * B[k][j];
-				}
-		  }
+    printf("Num procs = %d\n",nproc);
+  }
+  //clear C matrix 
+  if (myid==0){
+	  for (a = 0; a<DIM_N; a++)
+		  for (b = 0; b<DIM_N; b++)
+			  C[a][b] = 0;
+	  printf("Created the Matrices A and B\n");
 	}
-  //normalMultMatrix(A,B,C,DIM_N);
-
-  //stop timer
-  clock_gettime(CLOCK_REALTIME,&finish);
+	
+	//create the initial distributions
+	if (myid == 0){
+      //Allocate memory....this could get expensive pretty quickly
+    a11 = (double**) malloc(sizeof(double)*newsize);
+    a12 = (double**) malloc(sizeof(double)*newsize);
+    a21 = (double**) malloc(sizeof(double)*newsize);
+    a22 = (double**) malloc(sizeof(double)*newsize);
+    b11 = (double**) malloc(sizeof(double)*newsize);
+    b12 = (double**) malloc(sizeof(double)*newsize);
+    b21 = (double**) malloc(sizeof(double)*newsize);
+    b22 = (double**) malloc(sizeof(double)*newsize);
+    m1 = (double**) malloc(sizeof(double)*newsize);
+    m2 = (double**) malloc(sizeof(double)*newsize);
+    m3 = (double**) malloc(sizeof(double)*newsize);
+    m4 = (double**) malloc(sizeof(double)*newsize);
+    m5 = (double**) malloc(sizeof(double)*newsize);
+    m6 = (double**) malloc(sizeof(double)*newsize);
+    m7 = (double**) malloc(sizeof(double)*newsize);
+    t1 = (double**) malloc(sizeof(double)*newsize);
+    t2 = (double**) malloc(sizeof(double)*newsize);
+    t3 = (double**) malloc(sizeof(double)*newsize);
+    t4 = (double**) malloc(sizeof(double)*newsize);
+    t5 = (double**) malloc(sizeof(double)*newsize);
+    t6 = (double**) malloc(sizeof(double)*newsize);
+    t7 = (double**) malloc(sizeof(double)*newsize);
+    t8 = (double**) malloc(sizeof(double)*newsize);
+    t9 = (double**) malloc(sizeof(double)*newsize);
+    t10 = (double**) malloc(sizeof(double)*newsize);
+    
+    for (i=0; i < newsize; i++){
+      a11[i] = (double*) malloc(sizeof(double)*newsize);
+      a12[i] = (double*) malloc(sizeof(double)*newsize);
+      a21[i] = (double*) malloc(sizeof(double)*newsize);
+      a22[i] = (double*) malloc(sizeof(double)*newsize);
+      b11[i] = (double*) malloc(sizeof(double)*newsize); 
+      b12[i] = (double*) malloc(sizeof(double)*newsize); 
+      b21[i] = (double*) malloc(sizeof(double)*newsize);
+      b22[i] = (double*) malloc(sizeof(double)*newsize);    
+      m1[i] = (double*) malloc(sizeof(double)*newsize);
+      m2[i] = (double*) malloc(sizeof(double)*newsize);
+      m3[i] = (double*) malloc(sizeof(double)*newsize);
+      m4[i] = (double*) malloc(sizeof(double)*newsize);
+      m5[i] = (double*) malloc(sizeof(double)*newsize);
+      m6[i] = (double*) malloc(sizeof(double)*newsize);
+      m7[i] = (double*) malloc(sizeof(double)*newsize);
+      t1[i] = (double*) malloc(sizeof(double)*newsize);
+      t2[i] = (double*) malloc(sizeof(double)*newsize);
+      t3[i] = (double*) malloc(sizeof(double)*newsize);
+      t4[i] = (double*) malloc(sizeof(double)*newsize);
+      t5[i] = (double*) malloc(sizeof(double)*newsize);
+      t6[i] = (double*) malloc(sizeof(double)*newsize);
+      t7[i] = (double*) malloc(sizeof(double)*newsize);
+      t8[i] = (double*) malloc(sizeof(double)*newsize);
+      t9[i] = (double*) malloc(sizeof(double)*newsize);
+      t10[i] = (double*) malloc(sizeof(double)*newsize);
+    }
   
-  /*for (i=0; i<DIM_N; i++){
-    for (j=0; j<DIM_N; j++)
-      printf("%lf ",C[i][j]);
-    printf("\n");
-  }*/
+    splitMatrix(A,a11,a12,a21,a22,newsize);
+    splitMatrix(B,b11,b12,b21,b22,newsize);
+    
+    addMatrices(a11,a22,t1,newsize);
+    addMatrices(a21,a22,t2,newsize);
+    addMatrices(a11,a12,t3,newsize);
+    subMatrices(a21,a11,t4,newsize);
+    subMatrices(a12,a22,t5,newsize);
+    addMatrices(b11,b22,t6,newsize);
+    subMatrices(b12,b22,t7,newsize);
+    subMatrices(b21,b11,t8,newsize);
+    addMatrices(b11,b12,t9,newsize);
+    addMatrices(b21,b22,t10,newsize);
+    printf("Sending....\n\n");
+    for (i=0; i<newsize; i++){
+      MPI_Send(t2[i],newsize,MPI_DOUBLE,1,0,MPI_COMM_WORLD);
+      MPI_Send(b11[i],newsize,MPI_DOUBLE,1,1,MPI_COMM_WORLD);
+      MPI_Send(a11[i],newsize,MPI_DOUBLE,2,0,MPI_COMM_WORLD);
+      MPI_Send(t7[i],newsize,MPI_DOUBLE,2,1,MPI_COMM_WORLD);
+      MPI_Send(a22[i],newsize,MPI_DOUBLE,3,0,MPI_COMM_WORLD);
+      MPI_Send(t8[i],newsize,MPI_DOUBLE,3,1,MPI_COMM_WORLD);
+      MPI_Send(t3[i],newsize,MPI_DOUBLE,4,0,MPI_COMM_WORLD);
+      MPI_Send(b22[i],newsize,MPI_DOUBLE,4,1,MPI_COMM_WORLD);
+      MPI_Send(t4[i],newsize,MPI_DOUBLE,5,0,MPI_COMM_WORLD);
+      MPI_Send(t9[i],newsize,MPI_DOUBLE,5,1,MPI_COMM_WORLD);
+      MPI_Send(t5[i],newsize,MPI_DOUBLE,6,0,MPI_COMM_WORLD);
+      MPI_Send(t10[i],newsize,MPI_DOUBLE,6,1,MPI_COMM_WORLD);
+    }
 
-  //calculate time taken
-  ntime = finish.tv_nsec - start.tv_nsec;
-  stime = (long) finish.tv_sec - (long) start.tv_sec;
-  printf("Non-Strassen Time taken = %lf \n", (double) stime + ((double) ntime)/1e9);
+    //Start TimerMPI_COMM_WORLD
+    stime = MPI_Wtime();
+    
+    strassenMultMatrix(t1,t6,m1,newsize);
+    
+    printf("Mult done, receiving.... \n");
+    for (i=0; i<newsize; i++){
+      MPI_Recv(m2[i],newsize,MPI_DOUBLE,1,1,MPI_COMM_WORLD,NULL);
+      MPI_Recv(m3[i],newsize,MPI_DOUBLE,2,2,MPI_COMM_WORLD,NULL);
+      MPI_Recv(m4[i],newsize,MPI_DOUBLE,3,3,MPI_COMM_WORLD,NULL);
+      MPI_Recv(m5[i],newsize,MPI_DOUBLE,4,4,MPI_COMM_WORLD,NULL);
+      MPI_Recv(m6[i],newsize,MPI_DOUBLE,5,5,MPI_COMM_WORLD,NULL);
+      MPI_Recv(m7[i],newsize,MPI_DOUBLE,6,6,MPI_COMM_WORLD,NULL);
+    }
+    printf("Done, receiving\n");
+    addMatrices(m1,m4,a11,newsize);
+    subMatrices(m5,m7,a12,newsize);
+    addMatrices(m3,m1,a21,newsize);
+    subMatrices(m2,m6,a22,newsize);
+    subMatrices(a11,a12,b11,newsize);
+    addMatrices(m3,m5,b12,newsize);
+    addMatrices(m2,m4,b21,newsize);
+    subMatrices(a21,a22,b22,newsize);
+    
+    catMatrix(C,b11,b12,b21,b22,newsize);
+
+    //Stop Timer
+    ntime =  MPI_Wtime();
+    
+    free(a11);free(a12);free(a21);free(a22);
+    free(b11);free(b12);free(b21);free(b22);
+    free(t1);free(t2);free(t3);free(t4);free(t5);
+    free(t6);free(t7);free(t8);free(t9);free(t10);
+    free(m1);free(m2);free(m3);free(m4);free(m5);free(m6);free(m7);
+
+	} else if (myid>0 && myid<7){
+	  t1 = (double**) malloc(sizeof(double)*newsize);
+    t2 = (double**) malloc(sizeof(double)*newsize);
+    t3 = (double**) malloc(sizeof(double)*newsize);
+	  for (i=0; i < newsize; i++){
+	    t1[i] = (double*) malloc(sizeof(double)*newsize);
+      t2[i] = (double*) malloc(sizeof(double)*newsize);
+      t3[i] = (double*) malloc(sizeof(double)*newsize);
+    }
+    //printf("No %d Receiving....\n\n",myid);
+    for (i=0; i < newsize; i++){
+      MPI_Recv(t1[i],newsize,MPI_DOUBLE,0,0,MPI_COMM_WORLD,NULL);
+      MPI_Recv(t2[i],newsize,MPI_DOUBLE,0,1,MPI_COMM_WORLD,NULL);
+    }
+    //printf("done receiving %d\n",myid);
+    strassenMultMatrix(t1,t2,t3,newsize);
+    //printf("Mult done, sending back %d\n",myid);
+    for (i=0; i < newsize; i++){
+      MPI_Send(t3[i],newsize,MPI_DOUBLE,0,myid,MPI_COMM_WORLD);
+    }
+    //printf("done sending back %d\n",myid);
+    free(t1);free(t2);free(t3);
+	}
+
+  /*if(myid==0){
+    for (i=0; i<DIM_N; i++){
+      for (j=0; j<DIM_N; j++)
+        printf("%lf ",B[i][j]);
+      printf("\n");
+    }
+  }*/
+  if(myid == 0)
+    printf("N = %d \tTime taken = %f \n", DIM_N, ntime-stime);
+          
+  //End MPI session
+  MPI_Finalize();
+  
+
 }
 /*****************************************************************************
  * Note that all following functions only deal with square matrices          *
